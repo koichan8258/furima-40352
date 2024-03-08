e5e1b98b3eb3c7fdfa1fe38e2e638522747dd0b1
koichan8258 <iohikarinohito2@gmail.com>
2024-03-08 17:26:01 +0900

    ActiveHashの導入、アソシエーションの入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :record
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
