73e171eb577d3dfd7ff7d00fbeeba1cd98f5d12a
koichan8258 <iohikarinohito2@gmail.com>
2024-03-08 14:37:16 +0900

    デプロイ調整２

--- db/migrate/20240304093817_devise_create_users.rb
+++ db/migrate/20240304093817_devise_create_users.rb
@@ -10,6 +10,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :first_name_furigana, null:false, default: ""
       t.date :birth, null:false
       t.timestamps null: false
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
     end
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
