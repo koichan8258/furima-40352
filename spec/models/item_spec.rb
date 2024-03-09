2108166535b3d7975f85c58b6582555326e36e2c
koichan8258 <iohikarinohito2@gmail.com>
2024-03-09 19:39:38 +0900

    コード訂正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,6 +15,13 @@ RSpec.describe User, type: :model do
     end
 
     context '商品が出品できない場合' do
+
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
       it 'imageが空では登録できない' do
         @item.image = nil
         @item.valid?
