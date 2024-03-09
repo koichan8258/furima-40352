b44b522b501cc91f8f6a5365804f5382ca9afd5a
koichan8258 <iohikarinohito2@gmail.com>
2024-03-09 15:42:56 +0900

    ローティングとActiveHashの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,13 +12,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :explain, presence: true
-  validates :category, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :situation, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_charge, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :selling_price, numericality: { 
     greater_than_or_equal_to: 300, 
     less_than_or_equal_to: 9999999,
