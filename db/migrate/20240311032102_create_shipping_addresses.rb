0f3b9a8d6bb3400af7826c222b9edfe81ceca2eb
koichan8258 <iohikarinohito2@gmail.com>
2024-03-11 12:59:01 +0900

    マイグレーションの作成

--- db/migrate/20240311032102_create_shipping_addresses.rb
+++ db/migrate/20240311032102_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string :post_cord, null:false
+      t.integer :prefectuer, null:false
+      t.string :municipalities, null:false
+      t.string :street_address, null:false
+      t.string :building_name
+      t.string :telephon_number, null:false
+      t.references :record,foreign_key: true, null: false
       t.timestamps
     end
   end
