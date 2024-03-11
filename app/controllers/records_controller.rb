d0fd50ce4bc30893418efc205c0541866e102dac
koichan8258 <iohikarinohito2@gmail.com>
2024-03-11 15:27:05 +0900

    コントローラーの設定

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,4 +3,22 @@ class RecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @record_form = RecordShippingForm.new
   end
+
+  def create
+    @record_form = RecordShippingForm.new(record_params)
+    if @record_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def record_params
+    params.require(:record_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+  end
+
+
 end
