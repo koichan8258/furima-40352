d9920dc9d69e11a05526762a6772af86849955c7
koichan8258 <iohikarinohito2@gmail.com>
2024-03-12 15:09:55 +0900

    PAY.JPのテスト公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,5 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_8708ddec5845cc671c117747')
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
