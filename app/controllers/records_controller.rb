d5cc7e06809071718e8b507292644decf2cebc2a
koichan8258 <iohikarinohito2@gmail.com>
2024-03-12 17:38:54 +0900

    クライアントサイドで環境変数を読み込む

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,6 +2,7 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @record_shipping = RecordShipping.new
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_8708ddec5845cc671c117747')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
