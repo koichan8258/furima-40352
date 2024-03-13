571b7550acdcdaabcc2f62e4da894febc2de49aa
koichan8258 <iohikarinohito2@gmail.com>
2024-03-13 18:55:03 +0900

    Revert "Update card.js"
    This reverts commit 471a4f594cdb540b86e2601f3a15a7ee928cae8e.

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,6 @@
 const pay = () => {
-  const publicKey = gon.public_key;
-  console.log(gon.public_key);
-  const payjp = Payjp(publicKey);
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -10,9 +9,7 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-
   const form = document.getElementById('charge-form')
-  if (form) {
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
@@ -29,7 +26,6 @@ const pay = () => {
     });
     e.preventDefault();
   });
-}
 };
 
 window.addEventListener("turbo:load", pay);
