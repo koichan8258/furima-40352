69c37b5716aa295969a31efd2d081cf96da57cb6
koichan8258 <iohikarinohito2@gmail.com>
2024-03-14 16:36:13 +0900

    Revert "selling-price"
    This reverts commit 45966d8c1647a449eb3edd27d1ec4199b58a279b.

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -1,14 +1,24 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  if (priceInput) {
+    // 再計算関数を定義
+    const recalculate = () => {
+      const inputValue = parseFloat(priceInput.value);
 
-    const addTax = Math.floor(inputValue * 0.1);
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = addTax;
+      if (!isNaN(inputValue)) { 
+        const addTax = Math.floor(inputValue * 0.1);
+        const addTaxDom = document.getElementById("add-tax-price");
+        addTaxDom.textContent = addTax;
+        const profit = inputValue - addTax;
+        const profitDom = document.getElementById("profit");
+        profitDom.textContent = profit;
+      } else {
+        console.error("Invalid input: Not a number");
+      }
+    };
 
-    const profit = inputValue - addTax;
-    const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = profit;
-  });
+    priceInput.addEventListener("input", recalculate);
+
+    recalculate(); // ページロード時にも初回計算を実行
+  }
 });
