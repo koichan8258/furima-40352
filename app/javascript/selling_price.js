1716d3523e890bec1d0437c4a10a7c7e41abc8b0
koichan8258 <iohikarinohito2@gmail.com>
2024-03-14 12:28:02 +0900

    Update selling_price.js

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -2,15 +2,20 @@ window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   if (priceInput) {
     priceInput.addEventListener("input", () => {
-      const inputValue = priceInput.value;
+      const inputValue = parseFloat(priceInput.value);
 
-      const addTax = Math.floor(inputValue * 0.1);
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = addTax;
+      if (!isNaN(inputValue)) { 
+        const addTax = Math.floor(inputValue * 0.1);
+        const addTaxDom = document.getElementById("add-tax-price");
+        addTaxDom.innerHTML = addTax;
 
-      const profit = inputValue - addTax;
-      const profitDom = document.getElementById("profit");
-      profitDom.innerHTML = profit;
+        const profit = inputValue - addTax;
+        const profitDom = document.getElementById("profit");
+        profitDom.innerHTML = profit;
+      } else {
+
+        console.error("Invalid input: Not a number");
+      }
     });
   }
 });
