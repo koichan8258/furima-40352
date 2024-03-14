d5d341076ece0b11f107b7104c04a8300c5651e6
koichan8258 <iohikarinohito2@gmail.com>
2024-03-14 11:16:27 +0900

    Update selling_price.js

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -1,14 +1,16 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
 
-    const addTax = Math.floor(inputValue * 0.1);
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = addTax;
+      const addTax = Math.floor(inputValue * 0.1);
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = addTax;
 
-    const profit = inputValue - addTax;
-    const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = profit;
-  });
+      const profit = inputValue - addTax;
+      const profitDom = document.getElementById("profit");
+      profitDom.innerHTML = profit;
+    });
+  }
 });
