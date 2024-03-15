d9b4c1883ce55c161d1af50e0ad31c6782eaf49d
koichan8258 <iohikarinohito2@gmail.com>
2024-03-15 14:50:36 +0900

    販売利益

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -1,14 +1,29 @@
 window.addEventListener('turbo:load', () => {
+  const calculatePrice = () => {
+    const priceInput = document.getElementById("item-price");
+    const inputValue = parseFloat(priceInput.value); 
+
+    if (!isNaN(inputValue)) { 
+      const addTax = Math.floor(inputValue * 0.1);
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = addTax;
+
+      const profit = inputValue - addTax;
+      const profitDom = document.getElementById("profit");
+      profitDom.innerHTML = profit;
+    } else {
+
+      console.error("Invalid input: Not a number");
+    }
+  };
+
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  priceInput.addEventListener("input", calculatePrice);
 
-    const addTax = Math.floor(inputValue * 0.1);
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = addTax;
+  calculatePrice();
 
-    const profit = inputValue - addTax;
-    const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = profit;
+  window.addEventListener('turbo:render', () => {
+
+    calculatePrice();
   });
 });
